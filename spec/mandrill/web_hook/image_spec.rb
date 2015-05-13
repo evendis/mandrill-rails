e173df8b8181279512a928df13167302cbe9c232
Emil Sågfors <emil.sagfors@iki.fi>
2015-05-13 22:48:43 +0300

    Refactor Image spec

--- spec/mandrill/web_hook/image_spec.rb
+++ spec/mandrill/web_hook/image_spec.rb
@@ -3,26 +3,21 @@ require 'spec_helper'
 
 describe Mandrill::WebHook::Image do
 
-  [
-    {
-      :test_name => 'simple image file',
-      :given => { 'name' => 'c', 'type' => 'image/png', 'content' => 'iVBORw0KGgoAAAA....' },
-      :name => 'c',
-      :type => 'image/png',
-      :base64 => true,
-      :raw_content_matches => /^iVBORw0K/,
-      :decoded_content_matches => /^\x89PNG\r\n/n
-    }
-  ].each do |expectations|
-    context "when given #{expectations[:test_name]}" do
-      subject(:attachment) { Mandrill::WebHook::Image[expectations[:given]] }
-      it "exposes the file correctly" do
-        expect(attachment.name).to eql(expectations[:name])
-        expect(attachment.type).to eql(expectations[:type])
-        expect(attachment.base64).to eql(expectations[:base64])
-        expect(attachment.content).to match(expectations[:raw_content_matches])
-        expect(attachment.decoded_content).to match(expectations[:decoded_content_matches])
-      end
+  context 'when given the parameters for an image file' do
+    subject(:image) do
+      Mandrill::WebHook::Image[{
+        'name' => 'c',
+        'type' => 'image/png',
+        'content' => 'iVBORw0KGgoAAAA....'
+      }]
+    end
+
+    it 'exposes the file correctly' do
+      expect(image.name).to eql('c')
+      expect(image.type).to eql('image/png')
+      expect(image.base64).to eql(true)
+      expect(image.content).to match(/^iVBORw0K/)
+      expect(image.decoded_content).to match(/^\x89PNG\r\n/n)
     end
   end
 
