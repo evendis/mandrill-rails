5293020672365d01bef88aadba036e2aaeef7f62
estepan <es.prettyodd@gmail.com>
2015-07-26 15:17:38 +0300

    Update webhook processor Add sync webhook handling

--- lib/mandrill/web_hook/processor.rb
+++ lib/mandrill/web_hook/processor.rb
@@ -20,33 +20,11 @@ class Mandrill::WebHook::Processor
   # Command: processes all +mandrill_events+
   def run!
     mandrill_events.each do |raw_payload|
-      event_payload = wrap_payload(raw_payload)
-      handler = "handle_#{event_payload.event_type}".to_sym
-      if callback_host && callback_host.respond_to?(handler, true)
-        callback_host.send(handler,event_payload)
-      elsif self.respond_to?(handler)
-        self.send(handler,event_payload)
-      else
-        error_message = "Expected handler method `#{handler}` for event type `#{event_payload.event_type}`"
-        case on_unhandled_mandrill_events
-        when :ignore
-          # NOP
-        when :raise_exception
-          raise Mandrill::Rails::Errors::MissingEventHandler, error_message
-        else
-          Rails.logger.error error_message rescue nil
-        end
-      end
+      process_event(Mandrill::WebHook::EventDecorator[raw_payload])
     end
   end
 
-  # Returns a suitably ecapsulated +raw_event_payload+
-  def wrap_payload(raw_event_payload)
-    Mandrill::WebHook::EventDecorator[raw_event_payload]
-  end
-
   class << self
-
     # Returns true if +params+ sent to +original_url+ are authentic given +expected_signature+ and +mandrill_webhook_keys+.
     def authentic?(expected_signature, mandrill_webhook_keys, original_url, params)
       result = true
@@ -67,7 +45,35 @@ class Mandrill::WebHook::Processor
       end
       Base64.encode64("#{OpenSSL::HMAC.digest('sha1', webhook_key, signed_data)}").strip
     end
-
   end
 
+  private
+
+
+  # Returns event handler method name
+  def method_name(event_payload)
+    event_type = event_payload['type'].present? ? 'sync' : event_payload['event']
+
+    "handle_#{event_type}".to_sym
+  end
+
+  def process_event(event_payload)
+    handler = method_name(event_payload)
+
+    if callback_host && callback_host.respond_to?(handler, true)
+      callback_host.send(handler,event_payload)
+    elsif self.respond_to?(handler)
+      self.send(handler,event_payload)
+    else
+      error_message = "Expected handler method `#{handler}` for event type `#{event_payload.event_type}`"
+      case on_unhandled_mandrill_events
+      when :ignore
+        # NOP
+      when :raise_exception
+        raise Mandrill::Rails::Errors::MissingEventHandler, error_message
+      else
+        Rails.logger.error error_message rescue nil
+      end
+    end
+  end
 end
